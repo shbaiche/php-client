991d1f7c33930ac8c76574ca90c2032723bc074c
xinhua <xinhua@netlab.xyz>
2017-04-02 00:07:09 +0800

    update feie printer client

--- feieyun.com/FeiePrinter.php
+++ feieyun.com/FeiePrinter.php
@@ -1,6 +1,25 @@
 <?php
 class FeiePrinter {
 
+	private $sn = "";
+	private $key = "";
+	private $host = "";
+	private $proxy = null;
+
+	/**
+	 *
+	 * @param string $sn      打印机编号
+	 * @param string $key     密钥
+	 * @param string $host    购买机器后,联系客服获取即可
+	 *
+	 */
+	public function __construct($sn, $key, $host, $proxy = null) {
+		$this->sn = $sn;
+		$this->key = $key;
+		$this->host = $host;
+		$this->proxy = $proxy;
+	}
+
 	/**
 	 * 通过飞鹅云进行打印
 	 * 58mm的机器,一行打印16个汉字,32个字母
@@ -16,19 +35,16 @@ class FeiePrinter {
 	 * "<CODE>"为条形码,后面接12个数字
 	 *
 	 * @param string $content 打印内容(3500字节以内)
-	 * @param string $sn      打印机编号
-	 * @param string $key     密钥
-	 * @param string $host    购买机器后,联系客服获取即可
 	 * @param string $times   打印联数（同一订单，打印的次数）
 	 *
 	 * @return string
 	 * @link http://www.feieyun.com/document.jsp
 	 */
-	public static function print($content, $sn, $key, $host, $times = 1) {
-		$url = 'http://'.$host.'/FeieServer/printOrderAction';
+	public function print($content, $times = 1) {
+		$url = 'http://'.$this->host.'/FeieServer/printOrderAction';
 		$data = http_build_query([
-			'sn'=>$sn,
-			'key'=>$key,
+			'sn'=>$this->sn,
+			'key'=>$this->key,
 			'printContent'=>$content,
 			'times'=>$times
 		]);
@@ -39,9 +55,9 @@ class FeiePrinter {
 		curl_setopt($ch, CURLOPT_POST, TRUE);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 		curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-		if(defined('MY_PROXY_HOST') && defined('MY_PROXY_PORT')) {
-			curl_setopt($ch, CURLOPT_PROXY, MY_PROXY_HOST);
-			curl_setopt($ch, CURLOPT_PROXYPORT, MY_PROXY_PORT);
+		if(!empty($this->proxy)) {
+			curl_setopt($ch, CURLOPT_PROXY, $this->proxy['host']);
+			curl_setopt($ch, CURLOPT_PROXYPORT, $this->proxy['port']);
 		}
 		$result = curl_exec($ch);
 		curl_close($ch);
