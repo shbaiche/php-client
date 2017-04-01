81b32af48075dd5e3ae97ec4079d5216526677b1
xinhua <xinhua@netlab.xyz>
2017-04-01 23:17:22 +0800

    add sms client

---
+++ yuntongxun.com/SMSClient.php
@@ -0,0 +1,46 @@
+<?php
+class SMSClient {
+
+	/**
+	 * 通过容联云短信平台发送模板短信
+	 *
+	 * @param string $to           短信接收端手机号码集合，用英文逗号分开，每批发送的手机号数量不得超过200个
+	 * @param string $appId        应用Id
+	 * @param string $templateId   模板Id
+	 * @param array  $datas        内容数据，用于替换模板中{序号}
+	 * @param string $accountSid   开发者主账户ACCOUNT SID（登陆官网在管理控制台获取）
+	 * @param string $accountToken 账户授权令牌
+	 *
+	 * @return string
+	 * @link http://www.yuntongxun.com/doc/rest/sms/3_2_2_2.html
+	 */
+	public static function send($to, $appId, $templateId, $datas, $accountSid, $accountToken) {
+		$batch = date('YmdHis', time());
+		$sig =  strtoupper(md5($accountSid . $accountToken . $batch));
+		$authen = base64_encode($accountSid . ":" . $batch);
+		$url = "https://app.cloopen.com:8883/2013-12-26/Accounts/{$accountSid}/SMS/TemplateSMS?sig={$sig}";
+		$headers = [
+			'Accept:application/json',
+			'Content-Type:application/json;charset=utf-8',
+			"Authorization:{$authen}"
+		];
+		$data = json_encode([
+			'to'=>$to,
+			'appId'=> $appId,
+			'templateId'=> $templateId,
+			'datas'=> $datas
+		]);
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($ch, CURLOPT_POST, TRUE);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+		$result = curl_exec($ch);
+		curl_close($ch);
+		return $result;
+	}
+
+}
